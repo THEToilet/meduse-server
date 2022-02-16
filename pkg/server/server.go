9cf869c978a1aa30baf11d5cdb305d6e413622e9
THEToilet <bp18015@shibaura-it.ac.jp>
2022-02-16 19:18:06 +0900

    :sparkles:  TCPサーバのプロトタイプ作成

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -6,17 +6,46 @@ import (
 )
 
 func NewServer(port string, logger *zerolog.Logger) {
-
-	listener, err := net.Listen("tcp", ":"+port)
+	// NOTE: IPv4のみ
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", ":"+port)
 	if err != nil {
 		logger.Fatal().Interface("server down", "d").Msg("")
 	}
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		logger.Fatal().Interface("server down", "d").Msg("")
+	}
+
 	logger.Info().Str("Addr", port).Msg("Serve is running")
 
-	for{
+	for {
+
 		conn, err := listener.Accept()
 		if err != nil {
-			logger.Fatal().Interface("server down", "d").Msg("")
+			logger.Err(err).Msg(err.Error())
+			continue
+		}
+
+		for {
+			data := make([]byte, 128)
+			length, err := conn.Read(data)
+			if err != nil {
+				logger.Err(err).Msg(err.Error())
+				break
+			}
+
+			if length == 0 {
+				logger.Info().Str("", "d").Msg("connection is closed")
+				break
+			} else {
+				n, err := conn.Write([]byte("--"))
+				logger.Info().Str("", "d").Msg(string(n))
+				if err != nil {
+					logger.Err(err).Msg(err.Error())
+					break
+				}
+			}
 		}
 	}
 }
